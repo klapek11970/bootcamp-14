9d5cce99454a738ec286b8074b83d9fe7c0a3a4e
Patryk Zahorski <maarcane@gmail.com>
2019-02-04 15:56:39 +0100

    add google map with 3 cords

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 'use strict';
+//key: AIzaSyCV5qFMiOUGRXAL67rB-D18TVEWPjYNGVI
+
+///////////////////
+// Google Map
+var markers = []
+
+var locations = {
+	uluru : {lat: -25.344, lng: 125},
+	mark1 : {lat: -25.344, lng: 130},
+	mark2 : {lat: -25.344, lng: 135}
+}
+
+function initMap() {
+	
+  // The map, centered at Uluru
+  var map =
+	new google.maps.Map(document.getElementById('map'), {zoom: 4, center: locations.uluru});
+  
+  var key
+	for (key in locations){
+		markers.push(new google.maps.Marker({position: locations[key], map: map})) // locations[key] ?!?! czemu jako array a nie jako obiekt obj.key
+	}
+}
+
+///////////////////
+// template
 
 var templateItem = document.getElementById('template-carousel-item').innerHTML;
 
@@ -8,6 +34,7 @@ var listItems = '';
 
 for(var i = 0; i < carouselData.length; i++){
 	console.log(carouselData);
+	carouselData[i].id = i
 	listItems += Mustache.render(templateItem, carouselData[i]);
 }
 
