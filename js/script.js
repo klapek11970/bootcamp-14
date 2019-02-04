bb8e498b51538d38fdd1388962c4e8b7d43be4e0
Patryk Zahorski <maarcane@gmail.com>
2019-02-04 19:59:36 +0100

    connect carusel with googlemaps

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
 'use strict';
 //key: AIzaSyCV5qFMiOUGRXAL67rB-D18TVEWPjYNGVI
 
+var selectid = 0
+
 ///////////////////
 // Google Map
-//var markers = []
+var markers = []
+var map
 
 var locations = {
-	uluru : {lat: -25.344, lng: 125},
+	Uluru : {lat: -25.344, lng: 125},
 	Wasaw : {lat: 52.232855, lng: 20.9211131},
 	Berlin : {lat: 52.5067614, lng: 13.2846508},
-	W : {lat: 47.2511082, lng: -123.1246739},
-	loc4 : {lat: 55.5807482, lng: 36.82515}
+	Waszyngton : {lat: 47.2511082, lng: -123.1246739},
+	Moscow : {lat: 55.5807482, lng: 36.82515}
 }
 
 function initMap() {
-	
   // The map, centered at Uluru
-  var map =
-	new google.maps.Map(document.getElementById('map'), {zoom: 4, center: locations.uluru});
-  
-  var key
-	for (key in locations){
-		var marker = new google.maps.Marker({position: locations[key], map: map}) // locations[key] ?!?! czemu jako array a nie jako obiekt obj.key
-		//markers.push(marker)
-		marker.addListener('click', function(){
-			info.innerHTML = 'You clicked '+key;
-		});	
+  map =
+	new google.maps.Map(document.getElementById('map'), {zoom: 4, center: locations.Wasaw});
+
+  var city
+	for (city of Object.keys(locations)){
+		var marker = new google.maps.Marker({position: locations[city], map: map,city: city})
+		var id = markers.push(marker)-1
+		marker.id = id
+		
+		marker.addListener('click', function() {
+			event.preventDefault();
+			selectid = this.id // dont centre map after select marker
+			flkty.select(this.id);
+			//window.alert("you pressed at "+this.city+" marker")
+			console.log(this.id,this.city)
+		});
 	}
+	
 }
+
 
 ///////////////////
 // template
@@ -65,3 +75,54 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+/*
+var cells = document.querySelectorAll('.container img')
+
+for (var key of cells){
+	key.addEventListener('click', function(event){
+		event.preventDefault();
+		console.log(!maplock);
+		
+		if(!maplock){
+			//console.log( locations[markers[Flickity.data( carousel ).selectedIndex].city] );
+			map.panTo( locations[markers[Flickity.data( carousel ).selectedIndex].city] );
+			map.setZoom(5);
+		}
+	});
+}
+
+//dont change location if mousedown and moved
+var maplock = false
+
+key.addEventListener('click', function(event){
+	event.preventDefault();
+	console.log(!maplock);
+	
+	if(!maplock){
+		//console.log( locations[markers[Flickity.data( carousel ).selectedIndex].city] );
+		map.panTo( locations[markers[Flickity.data( carousel ).selectedIndex].city] );
+		map.setZoom(5);
+	}
+});
+
+
+flkty.on( 'pointerMove', function() {
+	maplock = true
+	//console.log('pointerMove')
+});
+
+flkty.on( 'pointerUp', function() {
+	//console.log('pointerUp')
+	window.setTimeout(function(){
+			maplock = false
+	}, 50);
+});
+*/
+
+flkty.on( 'select', function() {
+	if( selectid !== Flickity.data( carousel ).selectedIndex ){ // dont centre map after select marker
+		map.panTo( locations[markers[Flickity.data( carousel ).selectedIndex].city] );
+		map.setZoom(5);
+	}
+});
+
